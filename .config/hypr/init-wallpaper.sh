21716cea8235806675a805c9d3a03dd93960f79f
dvanhecke <duartevanhecke@gmail.com>
2025-01-15 10:07:39 +0100

    updates

--- .config/hypr/init-wallpaper.sh
+++ .config/hypr/init-wallpaper.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 directory=~/Pictures/Wallpapers
-monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
+cache_file=~/.cache/current_wallpaper
+
+if [ ! -f "$cache_file" ]; then
+    touch "$cache_file"
+fi
+
+current_wallpaper=$(head -n1 $cache_file)
 
 if [ -d "$directory" ]; then
     random_background=$(ls $directory/* | shuf -n 1)
+    if [ "$current_wallpaper" = "$random_background" ]; then
+        exit 0
+    fi
 
-    echo "setting wallpaper"
-
+    echo "$random_background" > "$cache_file"
     wal -n -i $random_background
     swww img $random_background --transition-type=any --transition-duration=2
     sleep 2
     pywal-discord
+    pywalfox update
     pywal-spicetify marketplace
     gsettings set org.gnome.desktop.interface gtk-theme "wal"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+    notify-send "System" "Wallpaper setted, and themed " -i $random_background
 fi
